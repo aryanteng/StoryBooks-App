4880a4eba1082f08b3f59108a9c2bb9b8999f78f
aryanteng <aryanteng892@gmail.com>
2022-07-15 20:38:52 +0530

    start google login

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require("path");
 
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 3000;
 
