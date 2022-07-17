b80ff312bc993e94f94467cc7b1a93c27fd9af71
aryanteng <aryanteng892@gmail.com>
2022-07-17 16:09:03 +0530

    Story Model

--- routes/auth.js
+++ routes/auth.js
@@ -10,10 +10,17 @@ router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 // @route GET /auth/google/callback
 router.get(
   "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/" }),
+  passport.authenticate("google", { failureRedirect: "/", scope: ["email"] }),
   (req, res) => {
     res.redirect("/dashboard");
   }
 );
+
+// @desc Logout user
+// @route /auth/logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
 module.exports = router;
